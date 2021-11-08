e339eea65339f79876653bdd94277da5cbb6acf6
Rayhan <nofussdufus@gmail.com>
2021-11-08 19:24:06 +0000

    Fix linters errors

--- static/js/index.js
+++ static/js/index.js
@@ -1,35 +1,27 @@
-window.addEventListener("resize",()=>{ window.location.reload()});
-
-const main_content = document.querySelector("main");
-const nav_mobile = document.querySelector(".nav-mobile");
-const nav_links = document.querySelector(".nav-links");
-
-if(window.innerWidth < 992){
-  add_mobile_events();
+window.addEventListener('resize', () => { window.location.reload(); });
+const navMobile = document.querySelector('.nav-mobile');
+const navLinks = document.querySelector('.nav-links');
+function showMenu() {
+  navMobile.style.display = 'none';
+  navLinks.classList.remove('drop-up');
+  navLinks.classList.add('drop-down');
+  navLinks.style.display = 'flex';
 }
-
-function add_mobile_events() {
-  document.querySelector(".menu-open").addEventListener("click", ()=>show_menu());
-  document.querySelector(".menu-close").addEventListener("click", ()=>hide_menu());
-  document.querySelectorAll(".section-link").forEach(link=>{
-    link.addEventListener("click", ()=>hide_menu());
+function hideMenu() {
+  navLinks.classList.remove('drop-down');
+  navLinks.classList.add('drop-up');
+  setTimeout(() => {
+    navMobile.style.display = 'flex';
+    navLinks.style.display = 'none';
+  }, 1000);
+}
+function addEvents() {
+  document.querySelector('.menu-open').addEventListener('click', () => showMenu());
+  document.querySelector('.menu-close').addEventListener('click', () => hideMenu());
+  document.querySelectorAll('.section-link').forEach((link) => {
+    link.addEventListener('click', () => hideMenu());
   });
 }
-
-function show_menu() {
-  nav_mobile.style.display = "none";
-  nav_links.classList.remove("drop-up");
-  nav_links.classList.add("drop-down");
-  nav_links.style.display = "flex";
-  main_content.style.pointerEvents = "none";
-  main_content.style.scrollBehavior = "none";
-}
-
-function hide_menu() {
-  nav_links.classList.remove("drop-down");
-  nav_links.classList.add("drop-up");
-  setTimeout(() => {
-    nav_mobile.style.display = "flex"
-    nav_links.style.display = "none"
-  }, 1000); 
+if (window.innerWidth < 992) {
+  addEvents();
 }
