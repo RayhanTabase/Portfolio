b40f6f684ec87db71d94875d39024e4aa515352e
Anselem <anselemodims@gmail.com>
2021-11-09 14:02:41 +0100

    Added card model to the popUp.js file for modal popup

--- static/js/popUp.js
+++ static/js/popUp.js
@@ -1,16 +1,34 @@
 
-const projects = {
-  0:{
-    'title':'Multi-Post Stories <br> Gain+Glory',
-    'description':"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    'tags':['Ruby on rails','css','JavScript','html'],
-    'image':""
-
+const projects = [
+  {
+  title1:'Multi-Post Stories',
+  title2: ' Gain+Glory',
+  description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    tags:['Ruby on rails','css','JavScript','html'],
+    image:'./static/images/Projects/img-placeholder.png'
   },
-  1:{},
-  2:{},
-  3:{}
-}
+  {
+    title1:'Tonic',
+    title2: ' Gain+Glory',
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      tags:['Ruby on rails','css','JavScript','html'],
+      image:'./static/images/Projects/img-placeholder.png'
+  },
+  {
+    title1:'Uber Navigation',
+    title2: ' Gain+Glory',
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      tags:['Ruby on rails','css','JavScript','html'],
+      image:'./static/images/Projects/img-placeholder.png'
+  },
+  {
+    title1:'Facebook 360',
+    title2: ' Gain+Glory',
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      tags:['Ruby on rails','css','JavScript','html'],
+      image:'./static/images/Projects/img-placeholder.png'
+  }
+]
 
 
 
