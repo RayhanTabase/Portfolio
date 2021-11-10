b390ea901cd76caaa95c32ca241ff2c3f44cd1e7
Rayhan <nofussdufus@gmail.com>
2021-11-10 14:31:00 +0000

    Add email lowercase client side validation

--- static/js/validators.js
+++ static/js/validators.js
@@ -6,8 +6,8 @@ export function removeErrorMessage(){
   error_message.innerHTML = '';
 }
 export const validateEmail = (e) => {
-  const email = document.querySelector('input[name="email"]').value
-  if (email === '' || /[A-Z]/.test(email)) {
+  const email = document.querySelector('input[name="email"]')
+  if (email.value === '' || /[A-Z]/.test(email)) {
     e.preventDefault();
     showError('Email should contain only lowercase characters');
   } 
